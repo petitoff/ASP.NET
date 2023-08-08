d7f5a41f579c0356edbdd1b3f1277c4d5e7d7fe9
Błażej Domagała <petitoff@outlook.com>
2023-08-08 17:49:34 +0200

    implemented CreatePointOfInterest to create point of interest

--- CityInfo/CityInfo.API/Controllers/PointsOfInterestController.cs
+++ CityInfo/CityInfo.API/Controllers/PointsOfInterestController.cs
@@ -40,5 +40,43 @@ namespace CityInfo.API.Controllers
 
             return Ok(pointOfInterest);
         }
+
+        [HttpPost]
+        public ActionResult<PointOfInterestDto> CreatePointOfInterest(int cityId,
+            PointOfInterestForCreationDto pointOfInterestForCreationDto)
+        {
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(cities => cities.Id == cityId);
+
+            if (city == null)
+            {
+                return NotFound();
+            }
+
+            var maxPointOfInterestId = CitiesDataStore.Current.Cities
+                .SelectMany(cities => cities.PointsOfInterest)
+                .Max(p => p.Id);
+
+            var finalPointOfInterest = new PointOfInterestDto()
+            {
+                Id = ++maxPointOfInterestId,
+                Name = pointOfInterestForCreationDto.Name,
+                Description = pointOfInterestForCreationDto.Description
+            };
+
+            city.PointsOfInterest.Add(finalPointOfInterest);
+
+            return CreatedAtRoute("GetPointOfInterest", new
+            {
+                cityId = cityId,
+                id = finalPointOfInterest.Id
+            }, finalPointOfInterest);
+        }
+
     }
 }
